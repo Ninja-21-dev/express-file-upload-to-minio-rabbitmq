348819ffa176e5ffed2e32cf944d35440e98436b
Ninja-21-dev <ninja.cooldev@gmail.com>
2022-11-10 21:41:09 -0500

    rabbitmq-file

--- file-uploader.js
+++ file-uploader.js
@@ -1,86 +1,55 @@
 var Minio = require("minio");
+require("./rabbit");
 
-var mClient = new Minio.Client({
-    endPoint: 'play.min.io',
-    port: 9000,
-    useSSL: true,
-    accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG' 
+const mClient = new Minio.Client({
+  endPoint: "play.min.io",
+  port: 9000,
+  useSSL: true,
+  accessKey: "Q3AM3UQ867SPQQA43P2F",
+  secretKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
 });
 
+var file = "./Outgoing/example.pdf";
+var testBucket,
+  testObject = "testObject",
+  testUrl;
 
-var file = './Outgoing/example.pdf'
+mClient.listBuckets(function (e, buckets) {
+  if (e) return console.log(e);
+  testBucket = buckets[0];
 
-// mClient.makeBucket('testbucke2', 'us-east-1', function(err) {
-//     if (err) return console.log(err)
+  console.log("Usefull bucket is found.");
 
-//     console.log('Bucket created successfully in "us-east-1"')
-
-//     var metaData = {
-//         'Content-Type': 'application/ootet-stream',
-//         'X-Amz-Meta-Testing': 1234,
-//         'example': 5678
-//     }
-
-//    mClient.fPutObject('testbucket2', 'testobject1', file, metaData, function(err, etag) {
-//     if (err) return console.log(err);
-//     console.log('File uploaded successfully.: ' + etag);
-//    })
-// })
-
-var testBucket, testObject = "testObject";
-const listBucketsPromise = mClient.listBuckets();
-const getBucketName = mClient.listBuckets(function(e, buckets) {
-    if (e) return console.log(e)
-    testBucket =  buckets[0];
-    // testBucket = buckets[0]
-    // console.log('buckets :')
-    // console.log(testBucket)
-
-
-    // console.log(testUrl);
   var metaData = {
-    'Content-Type': 'application/ootet-stream',
-    'X-Amz-Meta-Testing': 123,
-    'example': 456
+    "Content-Type": "application/ootet-stream",
+    "X-Amz-Meta-Testing": 123,
+    example: 456,
   };
 
-    mClient.fPutObject(testBucket.name, testObject, file, metaData, function(err,etag) {
-        if (err) return console.log(err);
-        console.log('File uploaded successfully.:');
+  mClient.fPutObject(
+    testBucket.name,
+    testObject,
+    file,
+    metaData,
+    function (err, etag) {
+      if (err) return console.log(err);
 
-        var testUrl;
-        mClient.presignedGetObject(testBucket.name, testObject, 1000, function(e, presignedUrl) {
-                    if (e) return console.log(e);
-                    testUrl = presignedUrl;
-                    console.log(testUrl);
-                  });
-    });
-  });
-//   testBucket = getBucketName;
-//   console.log('testBucket :');
-//   console.log(testBucket);
-  
+      console.log("File uploaded successfully.");
 
+      mClient.presignedGetObject(
+        testBucket.name,
+        testObject,
+        1000,
+        function (e, presignedUrl) {
+          if (e) return console.log(e);
+          testUrl = presignedUrl;
 
+          console.log("Url:");
+          console.log(testUrl);
 
-//   var testUrl = mClient.presignedGetObject('testbucke2', 'testobject1', 1000, function(e, presignedUrl) {
-//         if (e) return console.log(e);
-//         return presignedUrl;
-
-//       });
-
-//       console.log(testUrl);
-// mClient.getObjectLegalHold('testbucke2', 'testobject1', {}, function(err, res) {
-//     if (err) {
-//       return console.log('Unable to get legal hold config for the object', err.message);
-//     }
-//     console.log('Get Legalhold config');
-//     console.log(res);
-//   });
-
-//   var presignedUrl = mClient.presignedGetObject('testbucke2', 'testobject1', 1000, function(e, presignedUrl) {
-//     if (e) return console.log(e);
-//     console.log('Get url');
-//     console.log(presignedUrl);
-//   });
+          startPublisher(testUrl);
+        }
+      );
+    }
+  );
+});
--- rabbit.js
+++ rabbit.js
@@ -1,116 +1,116 @@
-var amqp = rquire('amqplib/callback_api');
+var amqp = rquire("amqplib/callback_api");
 var amqpConn = null;
 
-function start() {
-    amqp.connect("amqplib/callback_api", function(err, conn) {
-        if(err) {
-            console.error("[AMQP]", err.message);
-            return setTimeout(start, 1000);
-        }
-
-        conn.on("error", function(err) {
-            if( err.message !== "Connection closing") {
-                console.error("[AMQP] conn error", err.message);
-            }
-        })
-
-        conn.on("close", function() {
-            console.error("[AMQP] reconnecting");
-            return setTimeout(start, 1000);
-          });
-
-        amqpConn = conn;
-        whenConnected();
-    });
-}
-
-function whenConnected() {
-    startPublisher();
-    startWorker();
-  }
-
-  var pubChannel = null;
-  var offlinePubQueue = [];
-  function startPublisher() {
-    amqpConn.createConfirmChannel(function(err, ch){
-        if (closeOnerr(err)) return;
-
-        ch.on("error", function(err) {
-            console.err("[AMQP] channel error", err.message);
-        });
-
-        ch.on("close", function() {
-          console.log("[AMQP] channel closed");
-        });
-        
-        pubChannel = ch;
-        while(true) {
-            var [exchange, routingKey, content] = offlinePubQueue.shift();
-            startPublisher(exchange, routingKey, content);
-        }
-    });
-  }
-
-  function publish(exchange, routingKey, content) {
-    try {
-      pubChannel.publish(exchange, routingKey, content, { persistent: true },
-                        function(err, ok) {
-                          if (err) {
-                            console.error("[AMQP] publish", err);
-                            offlinePubQueue.push([exchange, routingKey, content]);
-                            pubChannel.connection.close();
-                          }
-                        });
-    } catch (e) {
-      console.error("[AMQP] publish", e.message);
-      offlinePubQueue.push([exchange, routingKey, content]);
+function start(content) {
+  amqp.connect("amqp://localhost", function (err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return setTimeout(start, 1000);
     }
-  }
 
-
-  // A worker that acks messages only if processed successfully
-function startWorker() {
-    amqpConn.createChannel(function(err, ch) {
-      if (closeOnErr(err)) return;
-      ch.on("error", function(err) {
-        console.error("[AMQP] channel error", err.message);
-      });
-      ch.on("close", function() {
-        console.log("[AMQP] channel closed");
-      });
-  
-      ch.prefetch(10);
-      ch.assertQueue("minio", { durable: true }, function(err, _ok) {
-        if (closeOnErr(err)) return;
-        ch.consume("minio", processMsg, { noAck: false });
-        console.log("Worker is started");
-      });
-    });
-  }
-
-  function processMsg(msg) {
-    work(msg, function(ok) {
-      try {
-        if (ok)
-          ch.ack(msg);
-        else
-          ch.reject(msg, true);
-      } catch (ejobs) {
-        closeOnErr(e);
+    conn.on("error", function (err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
       }
     });
-  }
 
-  function work(msg, cb) {
-    console.log("PDF processing of ", msg.content.toString());
-    cb(true);
-  }
+    conn.on("close", function () {
+      console.error("[AMQP] reconnecting");
+      return setTimeout(start, 1000);
+    });
 
-  function closeOnErr(err) {
-    if (!err) return false;
-    console.error("[AMQP] error", err);
-    amqpConn.close();
-    return true;
-  }
+    amqpConn = conn;
+    whenConnectedcontent;
+  });
+}
 
-  publish("", "minio", new Buffer())
+function whenConnected(content) {
+  startPublisher(content);
+  startWorker(content);
+}
+
+var pubChannel = null;
+var offlinePubQueue = [];
+function startPublisher(param) {
+  amqpConn.createConfirmChannel(function (err, ch) {
+    if (closeOnerr(err)) return;
+
+    ch.on("error", function (err) {
+      console.err("[AMQP] channel error", err.message);
+    });
+
+    ch.on("close", function () {
+      console.log("[AMQP] channel closed");
+    });
+
+    pubChannel = ch;
+    while (true) {
+      var [exchange, routingKey, param] = offlinePubQueue.shift();
+      publish(exchange, routingKey, param);
+    }
+  });
+}
+
+function publish(exchange, routingKey, content) {
+  try {
+    pubChannel.publish(
+      exchange,
+      routingKey,
+      content,
+      { persistent: true },
+      function (err, ok) {
+        if (err) {
+          console.error("[AMQP] publish", err);
+          offlinePubQueue.push([exchange, routingKey, content]);
+          pubChannel.connection.close();
+        }
+      }
+    );
+  } catch (e) {
+    console.error("[AMQP] publish", e.message);
+    offlinePubQueue.push([exchange, routingKey, content]);
+  }
+}
+
+// A worker that acks messages only if processed successfully
+function startWorker() {
+  amqpConn.createChannel(function (err, ch) {
+    if (closeOnErr(err)) return;
+    ch.on("error", function (err) {
+      console.error("[AMQP] channel error", err.message);
+    });
+    ch.on("close", function () {
+      console.log("[AMQP] channel closed");
+    });
+
+    ch.prefetch(10);
+    ch.assertQueue("minio", { durable: true }, function (err, _ok) {
+      if (closeOnErr(err)) return;
+      ch.consume("minio", processMsg, { noAck: false });
+      console.log("Worker is started");
+    });
+  });
+}
+
+function processMsg(msg) {
+  work(msg, function (ok) {
+    try {
+      if (ok) ch.ack(msg);
+      else ch.reject(msg, true);
+    } catch (ejobs) {
+      closeOnErr(e);
+    }
+  });
+}
+
+function work(msg, cb) {
+  console.log("PDF processing of ", msg.content.toString());
+  cb(true);
+}
+
+function closeOnErr(err) {
+  if (!err) return false;
+  console.error("[AMQP] error", err);
+  amqpConn.close();
+  return true;
+}
